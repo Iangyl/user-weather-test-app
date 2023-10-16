9870c881f649894a71f8c1a26e7f03cf0519c6fc
Volodymyr Barybin <vohanchib@gmail.com>
2023-10-16 20:12:25 +0300

    proj init and created home page

---
+++ app/components/Logo/index.js
@@ -0,0 +1,28 @@
+import { useMemo } from 'react';
+import Image from 'next/image';
+import weatherIcon from '@/app/assets/icons/weather-logo.svg';
+
+import styles from './index.module.sass';
+
+const sign = 'the weather';
+
+const Logo = ({ mode }) => {
+  const [logo, logoIcon, logoSign] = useMemo(
+    () =>
+      mode === 'load'
+        ? ['logoLoad', 'logoIconLoad', 'logoSignLoad']
+        : ['logoStatic', 'logoIconStatic', 'logoSignStatic'],
+    [mode]
+  );
+  
+  return (
+    <figure className={styles[logo]}>
+      <Image className={styles[logoIcon]} src={weatherIcon} alt="" />
+      <figcaption className={styles[logoSign]}>
+        {sign}
+      </figcaption>
+    </figure>
+  );
+};
+
+export default Logo;
